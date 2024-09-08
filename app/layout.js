0294f543290d2c69a68d548250d4c7b283945cc6
astridboberg <heia@astridboberg.no>
2024-09-08 13:05:55 +0200

    added dark mode 🌚

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,10 @@
+import {Providers} from "./providers";
 import Link from "next/link";
 import localFont from 'next/font/local';
 import '../assets/scss/global.scss';
 import styles from '../assets/scss/layout.module.scss';
 import Wrapper from './wrapper.js'
+import LightSwitch from './light-switch.js'
 
 const interDisplay = localFont({
   src: [
@@ -19,14 +21,16 @@ export const siteTitle = 'Astrid Mathilde Boberg';
 export default function Layout({ children }) {
   return (
     <>
-    <html lang="en" className={interDisplay.className}>
+    <html lang="en" className={interDisplay.className + " light"}>
     <Wrapper>
+    <Providers>
     <header id={styles.header}>
     <h1 className={styles.site_title}><a href="/">{siteTitle}</a></h1>
     <nav className={styles.site_navigation}>
       <ul>
         <li key="work"><Link href="/">Work</Link></li>
-        <li key="resumee"><Link href="resumee">Resumee</Link></li>
+        <li key="resumee"><Link href="/about">About</Link></li>
+        <li key="lightswitch"><LightSwitch /></li>
       </ul>
     </nav>
     </header>
@@ -35,8 +39,9 @@ export default function Layout({ children }) {
     </main>
     <footer id={styles.footer}>
     <address rel="author">© Astrid Mathilde Boberg</address>
-    <Link href="colophon">Colophon</Link>
+    <Link href="/colophon">Colophon</Link>
     </footer>
+    </Providers>
     </Wrapper>
     </html>
     </>
